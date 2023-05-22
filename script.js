16e4af08b6a1a7cdacaad99ec78cf08d7810c5c6
TarkanV <carchaido@gmail.com>
2023-05-22 17:06:54 +0100

    Make main game logic and start game menu

--- script.js
+++ script.js
@@ -1,99 +1,166 @@
 /* eslint-disable prefer-const */
 /* eslint-disable prefer-template */
 /* eslint-disable no-unused-vars */
-
-const gameBoard = ((doc)=>{
-    const templateCell = doc.querySelector("#template-cell");
-    const boardNode = doc.querySelector(".container");
-    const board = [];
-    
-
-    function Cell(){   
-        let status = "empty";
-        const node = (() => {
-            const clone = templateCell.content.cloneNode(true).firstElementChild;
-            boardNode.appendChild(clone);
-            return clone;
-        }
-        )();
-
-        const setStatus = (newStatus) =>{
-            
-            node.textContent = newStatus.toUpperCase();
-            status = newStatus;
-            
-        };
-        const getStatus = () => status;
-        return{setStatus, getStatus};
-    } 
-    
-    const initialize = () =>{
-        for(let i = 0; i < 9; i+=1) {
-                board.push(Cell());
-                if(i >= 3 && i <= 5) 
-                    board[board.length - 1].setStatus("O");
-                else 
-                    board[board.length - 1].setStatus("X");
-                
-
-            }
-
-    };
-
-    const winningMoves = [
-        [1, 2, 3], // Row 1
-        [4, 5, 6], // Row 2
-        [7, 8, 9], // Row 3
-        [1, 4, 7], // Column 1
-        [2, 5, 8], // Column 2
-        [3, 6, 9], // Column 3
-        [1, 5, 9], // Diagonal 1
-        [3, 5, 7]  // Diagonal 2
-    ];
-    let winningMove = "";
-    let isWin;
-    let endGame = false;
-    
-      
-    function checkWin(you, cpu){
-        winningMoves.find((move) => {
-            let line = "";
-            
-            move.forEach((i) =>{
-                console.log("Case : " + i);
-                line += board[i-1].getStatus();
-                console.log(line);
-            });
-            
-            if(line.match(/[X|Y]{3}/)){
-                winningMove = move.join();
-                console.log("We Have A Solution! : " + winningMove);
-                let winner;
-                if(move[0] === you.mark) {isWin = true; winner = you.name; you.score+=1;}
-                else {isWin = false; winner = cpu.name; cpu.score+=1;}
-                endGame = true;
-                return true;   
-            }
-            return false;    
-        });
-    }  
-
-    return{initialize, checkWin};
-}
-)(document);
-
-function Player(name, mark){
+function Player(name, mark, nodeName) {
     let score = 0;
-    return{
-        get name(){ return name},
-        score,
-        mark,
+    let upScore = () => {
+      score += 1;
+      return score;
     };
-}
+    let node = document.querySelector(nodeName);
+    let isTurn = false;
+  
+    const playTurn = (cellNode, cells) => {
+      let index = Array.from(cellNode.parentNode.children).indexOf(cellNode) - 1;
+      cells[index].setStatus(mark);
+    };
+  
+    return {
+      get name() {
+        return name;
+      },
+      upScore,
+      mark,
+      node,
+      playTurn,
+    };
+  }
+  
+  function CPUPlayer(name, mark, nodeName) {
+    const prototype = Player(name, mark, nodeName);
+    const makeChoice = () => {};
+  
+    return { ...prototype };
+  }
 
-gameBoard.initialize();
-const you = Player("You", "x");
-const cpu = Player("CPU", "o");
-gameBoard.checkWin(you, cpu);
 
-console.log(you.name);
+
+const gameBoard = ((doc) => {
+  const templateCell = doc.querySelector("#template-cell");
+  const boardNode = doc.querySelector(".container");
+  const outcomeNode = doc.querySelector(".outcome");
+  const menuNode = doc.querySelector(".menu-box");
+  const startButton = doc.querySelector(".start-button");
+  const turnInfo = doc.querySelector("turn-info");
+  let player1;
+  let player2;
+  let players;
+  
+  function Cell() {
+    let status = "";
+    const node = (() => {
+      const clone = templateCell.content.cloneNode(true).firstElementChild;
+      boardNode.appendChild(clone);
+      return clone;
+    })();
+
+    const setStatus = (newStatus) => {
+      node.textContent = newStatus.toUpperCase();
+      status = newStatus;
+    };
+    const getStatus = () => status;
+    return { setStatus, getStatus, node };
+  }
+
+  const cells = [];
+  let currPlayerIDX = 0;
+
+  const initialize = () => {
+    if(cells.length > 0) cells.length = 0;
+    for (let i = 0; i < 9; i += 1) {
+      cells.push(Cell());
+      if (i >= 3 && i <= 5) cells[cells.length - 1].setStatus("");
+      else cells[cells.length - 1].setStatus("");
+    }
+  };
+
+  function setPlayerName(inputName){
+    let name = menuNode.querySelector(inputName).value;
+    document.querySelector("." + inputName.split("-")[1]).querySelector("h2").textContent = name;
+    return name;
+  }
+
+  startButton.addEventListener("click", () => {
+    menuNode.classList.toggle("hidden");
+    initialize();
+    
+    player1 = Player(setPlayerName("#name-player1"), "X");
+    player2 = Player(setPlayerName("#name-player2"), "O");
+    players = [player1, player2];
+    gameBoard.play(players);
+    // const cpu = CPUPlayer("CPU", "o");
+
+    console.log(player1.name);
+  });
+
+  const winningMoves = [
+    [1, 2, 3], // Row 1
+    [4, 5, 6], // Row 2
+    [7, 8, 9], // Row 3
+    [1, 4, 7], // Column 1
+    [2, 5, 8], // Column 2
+    [3, 6, 9], // Column 3
+    [1, 5, 9], // Diagonal 1
+    [3, 5, 7], // Diagonal 2
+  ];
+  let winningMove = "";
+  let isWin;
+  let endGame = false;
+
+  function checkWin(you, cpu) {
+    winningMoves.find((move) => {
+      let line = "";
+
+      move.forEach((i) => {
+        console.log("Case : " + i);
+        line += cells[i - 1].getStatus();
+        console.log(line);
+      });
+
+      if (line.match(/X{3}|O{3}/)) {
+        winningMove = move.join();
+        console.log("We Have A Solution! : " + winningMove);
+        let winner;
+        console.log("Move : " + move[0]);
+        if (line[0] === you.mark) {
+          isWin = true;
+          winner = you;
+        } else {
+          isWin = false;
+          winner = cpu;
+        }
+        winner.upScore();
+        outcomeNode.textContent = `${winner.name} wins!`;
+        
+        endGame = true;
+        return true;
+      }
+      return false;
+    });
+  }
+
+
+  function play(players) {
+    console.log(cells[0].node);
+    cells.forEach((cell) =>
+      cell.node.addEventListener("click", (e) => {
+        if(!cell.getStatus() && !endGame){
+            const player = players[currPlayerIDX];
+            player.playTurn(e.target, cells);
+            checkWin(...players);
+            currPlayerIDX = +(!currPlayerIDX);
+        }
+      })
+    );
+  }
+
+  return { initialize, checkWin, play };
+})(document);
+
+
+// Player constructor
+
+
+
+
+
